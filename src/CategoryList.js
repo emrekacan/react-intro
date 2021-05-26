0ba4176bfc3a1f2463aec3fc5e115f43e7dc2666
emrekacan <emrehit@gmail.com>
2021-05-26 13:27:48 +0300

    First api usage

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -3,10 +3,17 @@ import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class CategoryList extends Component {
   state = {
-    categories: [
-      { categoryId: 1, categoryName: 'Beverages' },
-      { categoryId: 2, categoryName: 'Condiments' }
-    ]
+    categories: []
+  };
+  componentDidMount() {
+    this.getCategories();
+  }
+  getCategories = () => {
+    fetch(
+      'https://my-json-server.typicode.com/emrekacan/react-intro/categories'
+    )
+      .then(response => response.json())
+      .then(data => this.setState({ categories: data }));
   };
   render() {
     return (
@@ -16,7 +23,7 @@ export default class CategoryList extends Component {
           {this.state.categories.map(category => (
             <ListGroupItem
               onClick={() => this.props.changeCategory(category)}
-              key={category.categoryId}
+              key={category.id}
             >
               {category.categoryName}
             </ListGroupItem>
